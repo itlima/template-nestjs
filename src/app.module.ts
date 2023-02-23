8803b7ccb72fbbea19ae2817da055b1514a154f1
itlima <italo.vinicius1998@gmail.com>
2023-02-23 07:33:46 -0300

    chore: adding health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppConfigModule } from './config/app/configuration.module';
 import { OpenAPIConfigModule } from './config/openapi/configuration.module';
+import { HealthModule } from './models/health/health.module';
 import { UsersModule } from './models/users/users.module';
 import { PostgresqlDatabaseProviderModule } from './providers/database/postgresql//provider.module';
 @Module({
@@ -11,6 +12,7 @@ import { PostgresqlDatabaseProviderModule } from './providers/database/postgresq
     PostgresqlDatabaseProviderModule,
     UsersModule,
     ConfigModule.forRoot(),
+    HealthModule,
   ],
   controllers: [],
   providers: [],
