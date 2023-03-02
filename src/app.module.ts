8e2a1c7cd5e2c3ec227d925944c28bba55f84dd9
itlima <italo.vinicius1998@gmail.com>
2023-03-02 08:59:18 -0300

    feat: configuring logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerMiddleware, logger } from 'common/middlewares/logger.middleware';
 import { AppConfigModule } from './config/app/configuration.module';
 import { OpenAPIConfigModule } from './config/openapi/configuration.module';
 import { HealthModule } from './models/health/health.module';
+import { UsersController } from './models/users/users.controller';
 import { UsersModule } from './models/users/users.module';
 import { PostgresqlDatabaseProviderModule } from './providers/database/postgresql//provider.module';
 @Module({
@@ -17,4 +19,11 @@ import { PostgresqlDatabaseProviderModule } from './providers/database/postgresq
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware, logger)
+      .exclude({ path: 'users', method: RequestMethod.GET })
+      .forRoutes(UsersController);
+  }
+}
